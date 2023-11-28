09058fbb0c40179b436559a51a0639cd54ec5495
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2023-11-28 22:35:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugaspertemuan13firebaseprasetyoedi/Note.kt
@@ -0,0 +1,7 @@
+package com.example.tugaspertemuan13firebaseprasetyoedi
+
+data class Note(var id: String = "",
+                var name: String = "",
+                var title: String = "",
+                var details: String = ""
+)
